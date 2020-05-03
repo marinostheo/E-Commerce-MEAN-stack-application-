ca1e1ecc0e0505e06f621fe140e7cc730eb65bce
marinostheo <marinostheo@yahoo.gr>
2020-05-03 13:56:46 +0300

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { AppnavComponent } from './appnav/appnav.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
+    AppnavComponent,
+    CartComponent,
+    CheckoutComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
